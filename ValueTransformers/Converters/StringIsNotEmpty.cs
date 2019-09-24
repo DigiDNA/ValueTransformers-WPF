3440ab3836ad7b567e48b5b843ab623bad086c81
macmade <macmade@xs-labs.com>
2019-09-24 18:39:40 +0200

    StringIsEmpty + StringIsNotEmpty

---
+++ ValueTransformers/Converters/StringIsNotEmpty.cs
@@ -0,0 +1,46 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2019, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ValueTransformers
+{
+	[ MarkupExtensionReturnType( typeof( IValueConverter ) ) ]
+	[ ValueConversion( typeof( object ), typeof( bool ) ) ]
+	public class StringIsNotEmpty: MarkupExtension, IValueConverter
+	{
+		public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+        {
+            return value is string s && s.Length > 0;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		{
+			throw new NotSupportedException();
+		}
+
+		private static StringIsNotEmpty Converter
+        {
+            get;
+            set;
+        }
+
+		public override object ProvideValue( IServiceProvider serviceProvider )
+		{
+			if( Converter == null )
+			{
+				Converter = new StringIsNotEmpty();
+			}
+
+			return Converter;
+		}
+    }
+}
