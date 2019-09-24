6e77b1f6a6d23c5b2ae351dda024a79fb8f33904
macmade <macmade@xs-labs.com>
2019-09-24 15:04:32 +0200

    IsNull + IsNotNull

---
+++ ValueTransformers/Converters/IsNotNull.cs
@@ -0,0 +1,49 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2019, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ValueTransformers
+{
+    namespace Converters
+    {
+	    [ MarkupExtensionReturnType( typeof( IValueConverter ) ) ]
+	    [ ValueConversion( typeof( object ), typeof( bool ) ) ]
+	    public class IsNotNull: MarkupExtension, IValueConverter
+	    {
+		    public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		    {
+			    return value != null;
+		    }
+
+		    public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		    {
+			    throw new NotSupportedException();
+		    }
+
+		    private static IsNotNull Converter
+            {
+                get;
+                set;
+            }
+
+		    public override object ProvideValue( IServiceProvider serviceProvider )
+		    {
+			    if( Converter == null )
+			    {
+				    Converter = new IsNotNull();
+			    }
+
+			    return Converter;
+		    }
+        }
+    }
+}
