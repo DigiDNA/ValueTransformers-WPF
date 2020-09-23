5ccd28ff1928794a9c8564087a06f2e8bec26579
macmade <macmade@xs-labs.com>
2020-09-23 12:44:04 +0200

    Added StringToInt

---
+++ UnitTests/Tests/StringToInt.cs
@@ -0,0 +1,71 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2019, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Windows;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StringToInt
+    {
+        private readonly ValueTransformers.StringToInt Converter = new ValueTransformers.StringToInt();
+
+        [TestMethod]
+        public void TestNull()
+        {
+            Assert.AreEqual( 0, this.Converter.Convert( null, typeof( int ), null, null ) );
+            Assert.AreEqual( 0, this.Converter.Convert( null, typeof( int ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            Assert.AreEqual( 0, this.Converter.Convert( "", typeof( int ), null, null ) );
+            Assert.AreEqual( 0, this.Converter.Convert( "", typeof( int ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestString()
+        {
+            Assert.AreEqual(  42, this.Converter.Convert( "42", typeof( int ), null, null ) );
+            Assert.AreEqual( -42, this.Converter.Convert( "-42", typeof( int ), null, null ) );
+            Assert.AreEqual(   0, this.Converter.Convert( "hello, world", typeof( int ), null, null ) );
+            Assert.AreEqual(   0, this.Converter.Convert( "42.0", typeof( int ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestBadType()
+        {
+            Assert.ThrowsException<ArgumentException>( () => this.Converter.Convert( null, typeof( object ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestConvertBackNull()
+        {
+            Assert.AreEqual( "", this.Converter.ConvertBack( null, typeof( string ), null, null ) );
+            Assert.AreEqual( "", this.Converter.ConvertBack( null, typeof( string ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestConvertBack()
+        {
+            Assert.AreEqual( "42",  this.Converter.ConvertBack(  42, typeof( string ), null, null ) );
+            Assert.AreEqual( "-42", this.Converter.ConvertBack( -42, typeof( string ), null, null ) );
+        }
+
+        [TestMethod]
+        public void TestConvertBackBadType()
+        {
+            Assert.ThrowsException<ArgumentException>( () => this.Converter.ConvertBack( null, typeof( object ), null, null ) );
+        }
+    }
+}
---
+++ ValueTransformers/Converters/StringToInt.cs
@@ -0,0 +1,72 @@
+﻿/*******************************************************************************
+ * Copyright (c) 2019, DigiDNA
+ * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
+ ******************************************************************************/
+
+using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ValueTransformers
+{
+	[MarkupExtensionReturnType( typeof( IValueConverter ) )]
+	[ValueConversion( typeof( object ), typeof( Visibility ) )]
+	public class StringToInt: MarkupExtension, IValueConverter
+	{
+		public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		{
+			if( targetType != typeof( int ) )
+			{
+				throw new ArgumentException();
+			}
+
+			if( value is string str )
+			{
+				try
+				{
+					return int.Parse( str );
+				}
+				catch
+				{}
+			}
+
+			return 0;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		{
+			if( targetType != typeof( string ) )
+			{
+				throw new ArgumentException();
+			}
+
+			if( value is int i )
+			{
+				return i.ToString();
+			}
+
+			return "";
+		}
+
+		private static StringToInt Converter
+		{
+			get;
+			set;
+		}
+
+		public override object ProvideValue( IServiceProvider serviceProvider )
+		{
+			if( Converter == null )
+			{
+				Converter = new StringToInt();
+			}
+
+			return Converter;
+		}
+	}
+}
