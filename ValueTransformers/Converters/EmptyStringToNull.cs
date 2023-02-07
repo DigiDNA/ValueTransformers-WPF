f0cbe702c75f420340e4b455e9929abe58a1ec03
macmade <macmade@xs-labs.com>
2023-02-07 17:46:39 +0100

    Added EmptyStringToNull

---
+++ ValueTransformers/Converters/EmptyStringToNull.cs
@@ -0,0 +1,71 @@
+﻿/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2021 DigiDNA - www.imazing.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ValueTransformers
+{
+    [ MarkupExtensionReturnType( typeof( IValueConverter ) ) ]
+    [ ValueConversion( typeof( object ), typeof( string ) ) ]
+    public class EmptyStringToNull: MarkupExtension, IValueConverter
+    {
+        public object? Convert( object? value, Type targetType, object? parameter, System.Globalization.CultureInfo? culture )
+        {
+            Helper.CheckTargetType( targetType, typeof( string ) );
+
+            if( value == null )
+            {
+                return null;
+            }
+
+            if( ( value is string str ) == false )
+            {
+                throw new ArgumentException( "Invalid value", nameof( value ) );
+            }
+
+            if( parameter is string param && param.ToLower() == "trim" )
+            {
+                str = str.Trim();
+            }
+
+            return string.IsNullOrEmpty( str ) ? null : str;
+        }
+
+        public object? ConvertBack( object? value, Type targetType, object? parameter, System.Globalization.CultureInfo? culture )
+        {
+            throw new NotSupportedException();
+        }
+
+        private static readonly Lazy< EmptyStringToNull > Converter = new Lazy< EmptyStringToNull >( () => new EmptyStringToNull() );
+
+        public override object ProvideValue( IServiceProvider serviceProvider )
+        {
+            return Converter.Value;
+        }
+    }
+}
